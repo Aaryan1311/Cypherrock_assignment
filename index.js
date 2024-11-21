b541a5e246a7fad94a614fd3c860b3064facbd6f
Aaryan Tripathi <ayt131102@gmail.com>
2024-11-21 14:55:54 +0530

    updating index.js file

--- index.js
+++ index.js
@@ -1,77 +1,104 @@
 const {
   createWallet,
   importWallet,
-  listW,
-  allets,
+  listWallets,
   client,
 } = require("./bitcoin");
-const yargs = require("yargs");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
-yargs
-  .command("create", "Create a new wallet", () => {}, createWallet)
+const argv = yargs(hideBin(process.argv))
   .command(
-    "import <mnemonic>",
-    "Import a wallet from a BIP39 mnemonic",
-    (yargs) => {
-      yargs.positional("mnemonic", {
-        type: "string",
-        describe: "The BIP39 mnemonic",
-      });
-    },
-    (argv) => {
-      importWallet(argv.mnemonic);
+    "create",
+    "Create a new wallet",
+    (yargs) => yargs,
+    () => {
+      createWallet();
     }
   )
-  .command("list", "List all wallets", () => {}, listWallets)
   .command(
-    "balance <walletId>",
-    "Get bitcoin balance of a wallet",
+    "import <mnemonic>",
+    "Import a wallet using a BIP39 mnemonic phrase",
     (yargs) => {
-      yargs.positional("walletId", {
+      yargs.positional("mnemonic", {
+        describe: "The mnemonic phrase to import the wallet",
         type: "string",
-        describe: "The ID of the wallet",
       });
     },
-    async (argv) => {
-      const balance = await client.getBalance(argv.walletId);
-      console.log(`Balance of wallet ${argv.walletId}: ${balance}`);
+    (args) => {
+      importWallet(args.mnemonic);
+    }
+  )
+  .command(
+    "list",
+    "Show all available wallets",
+    (yargs) => yargs,
+    () => {
+      listWallets();
+    }
+  )
+  .command(
+    "balance <walletId>",
+    "Retrieve the Bitcoin balance of a specific wallet",
+    (yargs) => {
+      yargs.positional("walletId", {
+        describe: "The unique identifier of the wallet",
+        type: "string",
+      });
+    },
+    async (args) => {
+      try {
+        const balance = await client.getBalance(args.walletId);
+        console.log(`Wallet ID: ${args.walletId}\nBalance: ${balance} BTC`);
+      } catch (error) {
+        console.error("Error fetching balance:", error.message);
+      }
     }
   )
   .command(
     "transactions <walletId>",
-    "Get list of bitcoin transactions of a wallet",
+    "Display all transactions for a specific wallet",
     (yargs) => {
       yargs.positional("walletId", {
+        describe: "The unique identifier of the wallet",
         type: "string",
-        describe: "The ID of the wallet",
       });
     },
-    async (argv) => {
-      const transactions = await client.listTransactions(argv.walletId);
-      console.log(
-        `Transactions of wallet ${argv.walletId}: ${JSON.stringify(
-          transactions,
-          null,
-          2
-        )}`
-      );
+    async (args) => {
+      try {
+        const transactions = await client.listTransactions(args.walletId);
+        console.log(
+          `Wallet ID: ${args.walletId}\nTransactions:\n${JSON.stringify(
+            transactions,
+            null,
+            2
+          )}`
+        );
+      } catch (error) {
+        console.error("Error fetching transactions:", error.message);
+      }
     }
   )
   .command(
     "address <walletId>",
-    "Generate an unused bitcoin address for a wallet",
+    "Generate a new Bitcoin address for the specified wallet",
     (yargs) => {
       yargs.positional("walletId", {
+        describe: "The unique identifier of the wallet",
         type: "string",
-        describe: "The ID of the wallet",
       });
     },
-    async (argv) => {
-      const address = await client.getNewAddress(argv.walletId);
-      console.log(`New address of wallet ${argv.walletId}: ${address}`);
+    async (args) => {
+      try {
+        const newAddress = await client.getNewAddress(args.walletId);
+        console.log(`Wallet ID: ${args.walletId}\nNew Address: ${newAddress}`);
+      } catch (error) {
+        console.error("Error generating address:", error.message);
+      }
     }
   )
   .help()
   .alias("help", "h")
-  .demandCommand(1, "You need at least one command before moving on")
-  .strict().argv;
+  .demandCommand(1, "Please specify at least one command.")
+  .strict()
+  .parse();
